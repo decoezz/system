06d7303bff2d77649c64e00f371bd08d1cda809c
decoezz <45102309+decoezz@users.noreply.github.com>
2019-01-02 22:45:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2078,7 +2078,11 @@ message.channel.awaitMessages(filte, { max: 1, time: 15000, errors: ['time'] })
         
   }
 });
-  
+
+client.on('ready',async () => { client.channels.find(ch => ch.id === "530122097242931247" && ch.type === 'voice').join(); }); //Toxic Codes
+})
+});
+
 client.on("message", message => {
     var prefix = "-";
     const command = message.content.split(" ")[0];
